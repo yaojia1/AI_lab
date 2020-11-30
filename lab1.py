055605fe36c3145d1b35c6c0af1de85d7a55d34a
yaojia1 <288167@supinfo.com>
2020-11-30 12:09:45 +0800

    4*4

--- lab1.py
+++ lab1.py
@@ -1,31 +1,34 @@
 OPEN_list=[]
 CLOSE_list=[]
 success_shape=[0,1,2,3,4,5,6,7,8]
+Main_deep=50
 
 class tree_Node(object):
-    def __init__(self, array: list, node=None, x_move=0, y_move=0, x_init=0, y_init=0):
+    def __init__(self, initlist, node=None, x_move=0, y_move=0, x_init=0, y_init=0):
         self.child_list = []
         self.child_num = []  # 里面是child的移动方向list
         self.success_child=None
         if node is None:
             '''是根节点'''
             self.parent_node = None
-            self.shape = array
+            self.shape = initlist
             self.pos = [x_init, y_init]
-            self.deep = 0
+            self.deep=0
+            print("root node generating...")
+            print(self.shape[0:3])
+            print(self.shape[3:6])
+            print(self.shape[6:9])
         else:
             self.parent_node = node
-            self.deep = node.deep + 1  # 节点深度
+            self.deep = node.deep +1   # 节点深度
             pos_ini = node.pos[:]  # 继承父节点空格位置
-            '''
-            传入移动方向，改变初始shape
-            '''
-            shape0 = array[:]
-            pos1 = [pos_ini[0] + x_move, pos_ini[1] + y_move]
-            shape0[pos_ini[1] * 3 + pos_ini[0]] = shape0[pos1[0] * 3 + pos1[1]]
-            shape0[pos1[0] * 3 + pos1[1]] = 0
-            self.shape = shape0
-            self.pos = pos1[:]
+            self.shape = initlist[:]
+            self.pos = [pos_ini[0] + x_move, pos_ini[1] + y_move]
+            print(pos_ini,"-->",self.pos," 方向：",[x_move,y_move])
+            print(self.shape[0:3])
+            print(self.shape[3:6])
+            print(self.shape[6:9])
+
         self.parent_move = [x_move, y_move]  # 父节点的移动方向，用于搜索时排除父节点
         self.cate = OPEN_list
         self.status = False  # 有无被搜索过
@@ -33,8 +36,19 @@ class tree_Node(object):
         if self.shape==success_shape:
             self.success = True
         else:self.success=False
+        self.diff=0
+        for i in range(9):
+            if self.shape[i]!=success_shape[i]:
+                self.diff+=1
+        if self.diff==0:
+            self.success=True
         # self.step=0单开一个计算
 
+    def show_node(self):
+        print("deep:",self.deep," diff:",self.diff)
+        print(self.shape[0:3])
+        print(self.shape[3:6])
+        print(self.shape[6:9])
 
     def move_node(self, x_move, y_move):
         '''
@@ -45,8 +59,8 @@ class tree_Node(object):
         '''
         shape0 = self.shape[:]
         pos1 = [self.pos[0] + x_move, self.pos[1] + y_move]  # 目标位置
-        shape0[self.pos[1] * 3 + self.pos[0]] = shape0[pos1[0] * 3 + pos1[1]]
-        shape0[pos1[0] * 3 + pos1[1]] = 0
+        shape0[self.pos[1] * 3 + self.pos[0]] = shape0[pos1[1] * 3 + pos1[0]]
+        shape0[pos1[1] * 3 + pos1[0]] = 0
         return shape0
 
     def add_child(self):  #
@@ -57,8 +71,9 @@ class tree_Node(object):
         :param num:一次添加几个
         :return:
         '''
+        print("parent node:")
+        self.show_node()
         num=3
-        success_child=False
         for move_x in range(-1, 2):
             for move_y in range(-1, 2):
                 '''
@@ -67,10 +82,12 @@ class tree_Node(object):
                 子节点没加过
                 没移动出九宫格
                 '''
-                if abs(move_x + move_y) == 1 and abs(move_x + self.parent_move[0]) + abs(move_y + self.parent_move[1]) \
+                if abs(move_x + move_y) == 1 \
+                        and abs(move_x + self.parent_move[0]) + abs(move_y + self.parent_move[1]) \
                         and [move_x, move_y] not in self.child_num \
                         and 2>=move_x + self.pos[0] >= 0 and 2>=move_y + self.pos[1] >= 0:
                     shape_child = self.move_node(move_x, move_y)
+                    print("child:",shape_child)
                     self.child_num.append([move_x, move_y])
                     newchild = tree_Node(shape_child, self, move_x, move_y)
                     self.child_list.append(newchild)
@@ -86,6 +103,8 @@ class tree_Node(object):
                     TODO 判断是否success
                     
             '''
+        return False
+
     def is_success(self):
         if self.shape==success_shape:
             self.success=True
@@ -100,10 +119,10 @@ class BinaryIndexTree(object):
     TODO 子节点加到open的前面还是后面还是排序在加
     TODO show函数：清屏，显示九宫格数字，每添加一个节点show一次，下方提示深度等和是否成功
     '''
-    def __init__(self, array: list,x,y):
+    def __init__(self, initlist,x,y):
         '''初始化树'''
         self.success_path = []
-        self.init_map = array
+        self.init_map = initlist
         self.end_map=[0,1,2,3,4,5,6,7,8]
         OPEN_list=[]
         CLOSE_list=[]
@@ -118,8 +137,8 @@ class BinaryIndexTree(object):
         else:
 
             while len(OPEN_list):
-                print("open:",OPEN_list)
-                print(node.shape)
+                #print("open:",OPEN_list)
+                #print(node.shape)
                 if node.success:
                     self.sucess_build(node)
                     break
@@ -129,6 +148,8 @@ class BinaryIndexTree(object):
                         break
                     else:
                         node = OPEN_list[0]
+                        #node.show_node()
+                        print("open 长度",len(OPEN_list),"close 长度：",len(CLOSE_list))
                         continue
                 if search_type == "deep":
                     if self.deep_search(node):
@@ -169,7 +190,10 @@ class BinaryIndexTree(object):
             return True
         else:
             if self.inclose(node):
+                print("in close")
+                del OPEN_list[0]
                 return False
+
             if node.add_child():
                 self.success_path.append(node.success_child)
                 #self.success_path.insert(0,node)
@@ -177,13 +201,27 @@ class BinaryIndexTree(object):
             else:
                 CLOSE_list.append(node)
                 del OPEN_list[0]
-                OPEN_list[0:0]=node.child_list
-
+                for item in node.child_list:
+                    if self.inopen(item):
+                        print("in open")
+                        continue
+                    if self.inclose(item):
+                        print("in close")
+                    OPEN_list.append(item)
+        print("open长度",len(OPEN_list))
+        #self.show_open()
         return False
 
     def inclose(self,node:tree_Node):
         i=1
         for nodes in CLOSE_list:
+            if nodes.shape == node.shape:
+                return i
+            i+=1
+        return False
+    def inopen(self,node:tree_Node):
+        i=1
+        for nodes in OPEN_list:
             if nodes.shape == node.shape:
                 return i
             i+=1
@@ -204,6 +242,9 @@ class BinaryIndexTree(object):
                 CLOSE_list.append(node)
                 del OPEN_list[0]
                 OPEN_list.extend(node.child_list)
+                print("open 表：")
+                self.show_open()
+
 
         return False
 
@@ -215,7 +256,7 @@ class BinaryIndexTree(object):
         else:
             if self.inclose(node):
                 '''
-                TODO 计算f值比较大小，修改parent
+                TODO 计算f值比较大小，修改parent,i-1是close中的位置
                 '''
                 return False
             if node.add_child():
@@ -230,40 +271,24 @@ class BinaryIndexTree(object):
                 del OPEN_list[0]
                 OPEN_list[0:0] = node.child_list
 
+
         return False
 
-    def show_map(self):
-        pass
+    def show_close(self):
+        for node in CLOSE_list:
+            node.show_node()
+
+    def show_open(self):
+        for node in OPEN_list:
+            node.show_node()
     '''
     
     '''
 
-    def lowbit(self, x: int) -> int:
-        return x & (-x)
-
-    def update(self, idx: int, val: int):
-        '''将原数组idx下标更新为val, 总时间O(log n)'''
-        prev = self.query(idx, idx + 1)    # 计算出原来的值
-        idx += 1
-        val -= prev    # val 是要增加的值
-        while idx < len(self._array):
-            self._array[idx] += val
-            idx += self.lowbit(idx)
-
-    def query(self, begin: int, end: int) -> int:
-        '''返回数组[begin, end) 的和'''
-        return self._query(end) - self._query(begin)
-
-    def _query(self, idx: int) -> int:
-        '''计算数组[0, idx)的元素之和'''
-        res = 0
-        while idx > 0:
-            res += self._array[idx]
-            idx -= self.lowbit(idx)
-        return res
 
 
-ss=BinaryIndexTree([2,0,3,1,8,4,7,6,5],0,1)
+
+ss=BinaryIndexTree([2,0,3,1,8,4,7,6,5],1,0)
 ss.main_search("broad")
 
 bb= [123, 321, 654, 456,1,2,3]
